cc070a3199c0f99f9896ede15ee321d93cb1722a
Robert Sapunarich <robertsapunarich@gmail.comgit config --global user.email robertsapunarich@gmail.com>
2015-10-13 22:08:10 +0000

    connected users to places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,7 @@
 class PlacesController < ApplicationController
-	
+
+	before_action :authenticate_user!, :only => [:new, :create]
+
 	def index
 		@places = Place.all.page(params[:page]).per(5)
 	end
@@ -9,7 +11,7 @@ class PlacesController < ApplicationController
 	end
 
 	def create
-		Place.create(place_params)
+		current_user.places.create(place_params)
 		redirect_to root_path
 	end
 

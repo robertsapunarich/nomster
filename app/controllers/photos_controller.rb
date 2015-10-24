ead813dbb6df097a9638aa2d023ba46bc6d78649
Robert Sapunarich <robertsapunarich@gmail.comgit config --global user.email robertsapunarich@gmail.com>
2015-10-24 00:14:48 +0000

    added simple_form_for @photo to show.html.erb created PhotosController and set photo parameters nested photos#create action within places

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,14 @@
+class PhotosController < ApplicationController
+  def create
+    @place = Place.find(params [:place_id])
+    @place.photos.create(photo_params)
+    redirect_to place_path(@place)
+  end
+
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:picture)
+  end
+end

dd43edb5fc040bcdff094aa33c76edebd523f2f7
Robert Sapunarich <robertsapunarich@gmail.comgit config --global user.email robertsapunarich@gmail.com>
2015-10-27 23:17:39 +0000

    defined send_comment_email method in comment model to send an email notification. used after_create callback to send_comment_email to place_owner whenever a new comment is created. commented out smtp configuration in development.rb until it is needed again.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
 class Comment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :place
-
+	after_create :send_comment_email
+	
 	RATINGS = {
 		'one star'		=> '1_star',
 		'two stars'		=> '2_stars',
@@ -12,5 +13,9 @@ class Comment < ActiveRecord::Base
 
 	def humanized_rating
 		RATINGS.invert[self.rating]
+	end
+
+	def send_comment_email
+		NotificationMailer.comment_added(self).deliver
 	end
 end
